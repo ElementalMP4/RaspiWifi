7cf4e14e3916d495f499daaabf3be84faa3a314b
ElementalMP4 <sebahudl@gmail.com>
2023-05-07 13:55:12 +0000

    Add some UI

---
+++ public/index.js
@@ -0,0 +1,20 @@
+function doHttpGet(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            callback(xhr.status, xhr.responseText);
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+function updateWifi() {
+    let ssid = document.getElementById("ssid-input");
+    let password = document.getElementById("psk-input");
+    doHttpGet(`/wifi?ssid=${encodeURIComponent(ssid)}&psk=${encodeURIComponent(password)}`, () => { });
+}
+
+function reboot() {
+    doHttpGet("/reboot", () => { });
+}
