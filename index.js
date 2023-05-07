23fb062678c9a888ebf05782d817b74b5eb25779
ElementalMP4 <sebahudl@gmail.com>
2023-05-07 13:51:02 +0000

    Added some reboot logic

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const app = express();
 var morgan = require('morgan');
 const fs = require("fs");
+var exec = require('child_process').exec;
 
 const port = 80;
 
 app.use(morgan("dev"));
 app.use(express.static('public'));
+
+function execute(command, callback) {
+    exec(command, function (error, stdout, stderr) { callback(stdout); });
+}
+
+function reboot() {
+    execute('shutdown -r now', function (callback) {
+        console.log(callback);
+    });
+}
 
 app.get("/", (req, res) => {
     res.redirect("/index.html");
@@ -20,6 +31,11 @@ app.get("/wifi", (req, res) => {
     fs.writeFileSync("/etc/wpa_supplicant/wpa_supplicant.conf", final, { encoding: 'utf8', flag: 'w' })
     res.send(final);
 });
+
+app.get("/reboot", (req,res) => {
+    res.send("OK");
+    reboot();
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
